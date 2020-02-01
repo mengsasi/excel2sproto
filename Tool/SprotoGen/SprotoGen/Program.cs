dcf6735808b4c358e799b6cac54788ba1dc06a17
mengsasi <mengsasi@outlook.com>
2020-02-01 11:06:53 +0800

    dir

--- Tool/SprotoGen/SprotoGen/Program.cs
+++ Tool/SprotoGen/SprotoGen/Program.cs
@@ -11,16 +11,19 @@ namespace SprotoGen {
             string curDir = Environment.CurrentDirectory;
             try {
                 LuaMgr.Instance.Init();
-                LuaMgr.Instance.SetEnv( Path.Combine( curDir, "lua_src" ) );
-                if( args.Length > 1 ) {
-                    if( args[0] == "-s" ) {
-                        LuaMgr.Instance.DoString( args[1] );
+                if( args.Length == 4 ) {
+                    if( args[0] == "-d" ) {
+                        LuaMgr.Instance.SetEnv( Path.Combine( args[1], "lua_src" ) );
                     }
-                    else if( args[0] == "-f" ) {
-                        LuaMgr.Instance.DoFile( args[1] );
+                    if( args[2] == "-s" ) {
+                        LuaMgr.Instance.DoString( args[3] );
+                    }
+                    else if( args[2] == "-f" ) {
+                        LuaMgr.Instance.DoFile( args[3] );
                     }
                 }
                 else {
+                    LuaMgr.Instance.SetEnv( Path.Combine( curDir, "lua_src" ) );
                     LuaMgr.Instance.DoFile( "main" );
                     var func = LuaMgr.Instance.Get<LuaFunction>( "Main" );
                     func.Call( args );
